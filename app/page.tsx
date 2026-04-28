c765adcf61154e51d9f4f5c08792398aafcf4679
Leandro <prorootinfo@gmail.com>
2026-04-28 13:45:23 +0100

    Error the open component Webapp

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
-import Footer from "@/components/Footer"
-import Header from "@/components/Header"
-import SwiperCarousel from "@/components/ImageCarousel"
-import Network from "@/components/Network"
 import Service from "@/components/pages/Service"
-import Printers from "@/components/Printers"
-import Product from "@/components/product"
-import Webapp from "@/components/Webapp"
 
 export default function Home() {
   return (
---
+++ app/webapp/page.tsx
@@ -0,0 +1,5 @@
+import Webapp from "@/components/Webapp"
+
+export default function WebappsPage() {
+  return <Webapp />
+}
